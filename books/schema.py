615ceea3a219b4a8d36a494219aa0c13b35fc565
saulhappy <646634@gmail.com>
2022-02-08 10:18:34 -0600

    add Books schema and urls

---
+++ books/schema.py
@@ -0,0 +1,16 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Books
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Books
+        fields = ("id", "title", "excerpt")
+
+class Query(graphene.ObjectType):
+    all_books = graphene.List(BookType)
+
+    def resolve_all_books(root, info):
+        return Books.objects.all()
+
+schema = graphene.Schema(query=Query)
